796ce894afd16a8e12ba234c3de9ae0648e6bdac
amaha2428 <amahagodspower@gmail.com>
2024-02-05 15:17:18 +0100

    New changes

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def qa_chain():
 
     prompt_template = """Given the following context and a question, generate an answer based on this context only.
     In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
-    If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+    If the answer is not found in the context, kindly state "Your request is beyound my scope of assessment." Don't try to make up an answer.
 
     CONTEXT: {context}
 
@@ -56,8 +56,9 @@ def qa_chain():
 
 
 if __name__ == '__main__':
-    chain = qa_chain()
-    print(chain('WHo is the HOD of computer science'))
+    create_vector_db()
+    # chain = qa_chain()
+    # print(chain('WHo is the HOD of computer science'))
     # pass
   
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from app import qa_chain
 
 st.title("LangChain Test 🌱")
-
+st.write('Ensure to add question mark (?) at the end oa every question')
 question = st.text_input("Question: ")
 
 if question:
